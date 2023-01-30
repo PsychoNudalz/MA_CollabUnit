f20cf2eb5fa6df866436b56d9e56e680c25a8192
Noah <noahhoyle@gmail.com>
2023-01-30 20:54:04 +0000

    Noah: MainMenu v1 finished
    =)
    It actually works damn

--- Assets/Noah/Scripts/MainMenuButtons.cs
+++ Assets/Noah/Scripts/MainMenuButtons.cs
@@ -1,25 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class MainMenuButtons : MonoBehaviour
 {
-
+    
+    // TO DO:
+    // MERGE THIS SCRIPT WITH THE MENU MANAGER SO IT'S ALL TOGETHER
+    // THIS IS A WASTE OF CODE TO PUT IT HERE
+    
+    
     [SerializeField] private float delayTimer;
-
-
-    [SerializeField] private AnimationCurve animCurve;
     [SerializeField] private GameObject menuFrame;
     [SerializeField] private float menuLeaveTimer;
     
-    public void PlayGame()
+    [SerializeField] public GameObject[] menuButtons;
+    [SerializeField] public GameObject optionsMenu;
+
+    
+    public void PlayGame(string sceneName)
     {
-        
+        if (sceneName == null)
+        {
+            Debug.Log("No scene name loaded");
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 
     public void Options()
     {
-        
+        LeanTween.scale(gameObject, new Vector3(0, 0, 0), 0.3f).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.scale(optionsMenu, new Vector3(1, 1, 1), 0.3f).setEase(LeanTweenType.easeInOutCubic);
+    }
+
+    public void CloseOptions()
+    {
+        LeanTween.scale(gameObject, new Vector3(1, 1, 1), 0.3f).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.scale(optionsMenu, new Vector3(0, 0, 0), 0.3f).setEase(LeanTweenType.easeInOutCubic);
     }
 
     public void QuitGame()
@@ -27,10 +50,18 @@ public class MainMenuButtons : MonoBehaviour
         StartCoroutine(QuitGameDelay());
     }
 
+    public void DisableButton()
+    {
+        foreach (GameObject button in menuButtons)
+        {
+            button.GetComponent<Button>().interactable = false;
+        }
+    }
+
     private IEnumerator QuitGameDelay()
     {
         // Quick animation mockup
-        LeanTween.move(menuFrame, new Vector3(-10, -500, 0), menuLeaveTimer).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.scale(gameObject, new Vector3(0, 0, 0), 0.3f).setEase(LeanTweenType.easeInOutCubic);
         yield return new WaitForSeconds(delayTimer);
         Application.Quit();
 
--- Assets/Noah/Scripts/MenuManager.cs
+++ Assets/Noah/Scripts/MenuManager.cs
@@ -14,27 +14,50 @@ public class MenuManager : MonoBehaviour
     // the code is shit. I KNOW IT IS 😤
 
     private bool introMenuActive;
-    [SerializeField] public GameObject introMenu, mainMenu;
+    [SerializeField] public GameObject introMenu, mainMenu, gameLogo, titleScreenText;
+    
+    [SerializeField] public GameObject[] menuButtons;
+
+    private bool canStartMenu;
 
     private void Start()
     {
         introMenu.SetActive(true);
         mainMenu.SetActive(false);
+        titleScreenText.SetActive(false);
+
+        canStartMenu = false;
+        StartCoroutine(IntroStart());
+
+        LeanTween.scale(gameLogo, new Vector3(1, 1, 1), 1.5f).setEase(LeanTweenType.easeInBounce);
     }
 
     private void Update()
     {
         if (Keyboard.current.anyKey.wasPressedThisFrame || Mouse.current.leftButton.wasPressedThisFrame || Mouse.current.rightButton.wasPressedThisFrame)
         {
-            ChangeMenu();
+            if(canStartMenu)
+                ChangeMenu();
         }
+    }
+
+    private IEnumerator IntroStart()
+    {
+        yield return new WaitForSeconds(2f);
+        canStartMenu = true;
+        titleScreenText.SetActive(true);
     }
 
     private void ChangeMenu()
     {
         introMenu.SetActive(false);
         mainMenu.SetActive(true);
-        LeanTween.scale(mainMenu, new Vector3(1, 1, 1), 0.3f).setEase(LeanTweenType.easeInBounce);
+
+        foreach (GameObject button in menuButtons)
+        {
+            LeanTween.scale(button, new Vector3(1, 1, 1), 0.3f).setEase(LeanTweenType.easeInCubic);
+        }
+        
     }
     
     
