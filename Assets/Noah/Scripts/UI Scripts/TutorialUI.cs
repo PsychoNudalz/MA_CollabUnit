f14226c90c915d79fb17a2a8fd0f340d20697460
Noah <noahhoyle@gmail.com>
2023-02-16 11:17:33 +0000

    Noah: New scene to start work on greybox for upper section of level
    Plus a few changes to UI

--- Assets/Noah/Scripts/UI Scripts/TutorialUI.cs
+++ Assets/Noah/Scripts/UI Scripts/TutorialUI.cs
@@ -8,8 +8,14 @@ public class TutorialUI : MonoBehaviour
     // Start is called before the first frame update
 
     public RectTransform tut1, tut2, tut3;
+
+    public PauseMenu pm;
+    
     void Start()
     {
+        pm.canBePaused = false;
+        Cursor.visible = true;
+        
         Time.timeScale = 0f;
         LeanTween.scale(gameObject.GetComponent<RectTransform>(), new Vector3(1, 1, 1), 0.3f).setEase(LeanTweenType.easeInOutCubic).setIgnoreTimeScale(true);
 
@@ -18,12 +24,12 @@ public class TutorialUI : MonoBehaviour
         LeanTween.alpha(tut1, 1, 0.5f).setIgnoreTimeScale(true).setDelay(2f);
         
         LeanTween.scale(tut2, new Vector3(1, 1, 1), 0.3f).setEase(LeanTweenType.easeInOutCubic)
-            .setIgnoreTimeScale(true).setDelay(6f);
-        LeanTween.alpha(tut2, 1, 0.5f).setIgnoreTimeScale(true).setDelay(6f);
+            .setIgnoreTimeScale(true).setDelay(5f);
+        LeanTween.alpha(tut2, 1, 0.5f).setIgnoreTimeScale(true).setDelay(5f);
         
         LeanTween.scale(tut3, new Vector3(1, 1, 1), 0.3f).setEase(LeanTweenType.easeInOutCubic)
-            .setIgnoreTimeScale(true).setDelay(12f);
-        LeanTween.alpha(tut3, 1, 0.5f).setIgnoreTimeScale(true).setDelay(12f);
+            .setIgnoreTimeScale(true).setDelay(8f);
+        LeanTween.alpha(tut3, 1, 0.5f).setIgnoreTimeScale(true).setDelay(8f);
     }
 
     // Update is called once per frame
@@ -37,6 +43,7 @@ public class TutorialUI : MonoBehaviour
     {
         Time.timeScale = 1f;
         LeanTween.scale(gameObject.GetComponent<RectTransform>(), new Vector3(0, 0, 0), 0.3f).setEase(LeanTweenType.easeInOutCubic).setIgnoreTimeScale(true);
+        pm.canBePaused = true;
     }
     
     
